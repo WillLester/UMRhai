1e779ee01ac59bac2961f33e8a60a8df28a5a13f
Will Lester <839754614@qq.com>
2015-10-27 20:23:25 +0800

    界面更新

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/MainFrame.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/MainFrame.java
@@ -3,6 +3,7 @@ package edu.nju.umr.ui;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame{
@@ -10,29 +11,16 @@ public class MainFrame extends JFrame{
 	static final int FRAME_Y = Toolkit.getDefaultToolkit().getScreenSize().height/10;
 	static final int FRAME_WIDTH = (int)(Toolkit.getDefaultToolkit().getScreenSize().width*0.8);
 	static final int FRAME_HEIGHT = (int)(Toolkit.getDefaultToolkit().getScreenSize().height*0.8);
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainFrame frame = new MainFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JDesktopPane desktop;
 
 	/**
 	 * Create the frame.
 	 */
 	public MainFrame() {
+		desktop = new JDesktopPane();
+		this.getContentPane().add(desktop);
 		setTitle("快递物流系统");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(FRAME_X,FRAME_Y,FRAME_WIDTH,FRAME_HEIGHT);
-		
 	}
 }
--- 3_Coding/UI/src/edu/nju/umr/ui/MainFrame.java
+++ 3_Coding/UI/src/edu/nju/umr/ui/MainFrame.java
@@ -3,6 +3,7 @@ package edu.nju.umr.ui;
 import java.awt.EventQueue;
 
 import javax.swing.JButton;
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,12 +14,14 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.Toolkit;
+import java.beans.PropertyVetoException;
 
 public class MainFrame extends JFrame {
 	static final int FRAME_X = Toolkit.getDefaultToolkit().getScreenSize().width/10;
 	static final int FRAME_Y = Toolkit.getDefaultToolkit().getScreenSize().height/10;
 	static final int FRAME_WIDTH = (int)(Toolkit.getDefaultToolkit().getScreenSize().width*0.8);
-	static final int FRAME_HEIGHT = (int)(Toolkit.getDefaultToolkit().getScreenSize().width*0.8);
+	static final int FRAME_HEIGHT = (int)(Toolkit.getDefaultToolkit().getScreenSize().height*0.8);
+	private JDesktopPane desktop;
 	/**
 	 * Launch the application.
 	 */
@@ -28,6 +31,7 @@ public class MainFrame extends JFrame {
 				try {
 					MainFrame frame = new MainFrame();
 					frame.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -39,9 +43,12 @@ public class MainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public MainFrame() {
+		desktop = new JDesktopPane();
+		this.getContentPane().add(desktop);
 		setTitle("快递物流系统");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(FRAME_X,FRAME_Y,FRAME_WIDTH,FRAME_HEIGHT);
-		
+		FuncFrame funcFrame = new FuncFrame();
+		funcFrame.setVisible(true);
 	}
 }
