95cf4c64cd6596cd133972348aae490422273710
Will Lester <839754614@qq.com>
2015-10-28 17:05:18 +0800

    个人信息界面

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/PersonalInfoPanel.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/PersonalInfoPanel.java
@@ -2,6 +2,7 @@ package edu.nju.umr.ui;
 
 import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -9,6 +10,7 @@ public class PersonalInfoPanel extends JPanel{
 	public PersonalInfoPanel() {
 		setLayout(null);
 		this.setSize(MainFrame.FRAME_WIDTH, MainFrame.FRAME_HEIGHT);
+		System.out.println(this.getWidth() + " " + this.getHeight());
 		JLabel personalInfo = new JLabel("个人信息");
 		personalInfo.setFont(new Font("华文新魏", Font.PLAIN, 25));
 		personalInfo.setBounds(this.getWidth()/9, this.getHeight()/10, this.getWidth()/9, this.getHeight()/7);
@@ -33,6 +35,18 @@ public class PersonalInfoPanel extends JPanel{
 		mobile.setFont(new Font("华文新魏", Font.PLAIN, 20));
 		mobile.setBounds(this.getWidth()/9, this.getHeight()/10 + 5 * this.getHeight()/10, this.getWidth()/9, this.getHeight()/20);
 		add(mobile);
+		
+		JButton logoutButton = new JButton("注销");
+		logoutButton.setBounds(8 * this.getWidth()/10, this.getHeight()/20, this.getWidth()/18, this.getHeight()/20);
+		add(logoutButton);
+		
+		JButton exitButton = new JButton("退出");
+		exitButton.setBounds(7 * this.getWidth()/8, this.getHeight()/20, this.getWidth()/18, this.getHeight()/20);
+		add(exitButton);
+		
+		JButton button = new JButton("返回主界面");
+		button.setBounds(8 * this.getWidth()/10, 8*this.getHeight()/10, this.getWidth()/10, this.getHeight()/20);
+		add(button);
 
 	}
 }
--- 3_Coding/UI/src/edu/nju/umr/ui/PersonalInfoPanel.java
+++ 3_Coding/UI/src/edu/nju/umr/ui/PersonalInfoPanel.java
@@ -4,6 +4,9 @@ import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PersonalInfoPanel extends JPanel {
 
@@ -13,6 +16,7 @@ public class PersonalInfoPanel extends JPanel {
 	public PersonalInfoPanel() {
 		setLayout(null);
 		this.setSize(MainFrame.FRAME_WIDTH, MainFrame.FRAME_HEIGHT);
+		System.out.println(this.getWidth() + " " + this.getHeight());
 		JLabel personalInfo = new JLabel("个人信息");
 		personalInfo.setFont(new Font("华文新魏", Font.PLAIN, 25));
 		personalInfo.setBounds(this.getWidth()/9, this.getHeight()/10, this.getWidth()/9, this.getHeight()/7);
@@ -37,6 +41,18 @@ public class PersonalInfoPanel extends JPanel {
 		mobile.setFont(new Font("华文新魏", Font.PLAIN, 20));
 		mobile.setBounds(this.getWidth()/9, this.getHeight()/10 + 5 * this.getHeight()/10, this.getWidth()/9, this.getHeight()/20);
 		add(mobile);
+		
+		JButton logoutButton = new JButton("注销");
+		logoutButton.setBounds(8 * this.getWidth()/10, this.getHeight()/20, this.getWidth()/18, this.getHeight()/20);
+		add(logoutButton);
+		
+		JButton exitButton = new JButton("退出");
+		exitButton.setBounds(7 * this.getWidth()/8, this.getHeight()/20, this.getWidth()/18, this.getHeight()/20);
+		add(exitButton);
+		
+		JButton button = new JButton("返回主界面");
+		button.setBounds(8 * this.getWidth()/10, 8*this.getHeight()/10, this.getWidth()/10, this.getHeight()/20);
+		add(button);
 
 	}
 }
