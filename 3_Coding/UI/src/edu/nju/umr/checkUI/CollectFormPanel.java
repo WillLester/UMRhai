e129efdb5944715c17b9187992f622cbb739c37f
yyy2015 <2456700983@qq.com>
2015-11-01 21:08:08 +0800

    代码部分yyy界面更新

---
+++ 3_Coding/UI/src/edu/nju/umr/checkUI/CollectFormPanel.java
@@ -0,0 +1,43 @@
+package edu.nju.umr.checkUI;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+
+import edu.nju.umr.ui.Constants;
+
+import java.awt.Font;
+
+import javax.swing.JButton;
+/*
+ * yyy
+ * 20151029
+ * 成本收益表
+ */
+public class CollectFormPanel extends JPanel {
+	private JTable collectTable;
+
+	/**
+	 * Create the panel.
+	 */
+	public CollectFormPanel() {
+		setLayout(null);
+		
+		this.setSize(1104,621);
+		
+		JLabel nameLabel = new JLabel("成本收益表");
+		nameLabel.setFont(new Font("华文新魏", Font.PLAIN, 22));
+//		nameLabel.setBounds(this.getWidth()/2-this.getWidth()/14, this.getHeight()/20, this.getWidth()/7, this.getHeight()/15);
+		nameLabel.setBounds(this.getWidth()/2-Constants.LABEL_WIDTH/2, 0, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT_L);
+		add(nameLabel);
+		
+		collectTable = new JTable();
+		collectTable.setBounds(this.getWidth()/10, this.getHeight()/9, this.getWidth()/10*8, this.getHeight()/9*6);
+		add(collectTable);
+		
+		JButton out = new JButton("退出");
+		out.setBounds(this.getWidth()/10*9-Constants.BUTTON_WIDTH_S, this.getHeight()/10*8, Constants.BUTTON_WIDTH_S, Constants.BUTTON_HEIGHT);
+		add(out);
+
+	}
+}
