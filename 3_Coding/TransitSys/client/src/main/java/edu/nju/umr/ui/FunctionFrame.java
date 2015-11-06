bc142ed2219024feb0ed7c0c6c3f85ec0b0f4b95
EnterpriseCV <502101600@qq.com>
2015-11-06 20:54:11 +0800

    更新

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/FunctionFrame.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/FunctionFrame.java
@@ -7,6 +7,7 @@ import edu.nju.umr.ui.accountUI.CountPanel;
 import edu.nju.umr.ui.workOrgManUI.*;
 import edu.nju.umr.ui.stockUI.*;
 import edu.nju.umr.ui.userManUI.*;
+import edu.nju.umr.ui.orderNewUI.*;
 
 public class FunctionFrame extends JInternalFrame{
 	static final int FRAME_X = MainFrame.FRAME_WIDTH/20;
--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/userManUI/UserListPanel.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/userManUI/UserListPanel.java
@@ -77,10 +77,71 @@ public class UserListPanel extends JPanel {
 		exitButton.setBounds(895, 487, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
 		add(exitButton);
 		
-		JLabel userNameLabel=new JLabel("账号");
-		userNameLabel.setFont(new Font("宋体", Font.PLAIN, 12));
-		userNameLabel.setBounds(805,121, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
-		add(userNameLabel);
+		JLabel idLabel=new JLabel("账号");
+		idLabel.setFont(new Font("宋体", Font.PLAIN, 12));
+		idLabel.setBounds(805,121, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+		add(idLabel);
+		
+		JLabel passwordLabel=new JLabel("密码");
+		passwordLabel.setFont(new Font("宋体", Font.PLAIN, 12));
+		passwordLabel.setBounds(805,171, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+		add(passwordLabel);
+		
+		JLabel juriLabel=new JLabel("权限");
+		juriLabel.setFont(new Font("宋体", Font.PLAIN, 12));
+		juriLabel.setBounds(805,221, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+		add(juriLabel);
+		
+		JLabel nameLabel=new JLabel("姓名");
+		nameLabel.setFont(new Font("宋体", Font.PLAIN, 12));
+		nameLabel.setBounds(805,271, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+		add(nameLabel);
+		
+		JLabel orgLabel=new JLabel("机构");
+		orgLabel.setFont(new Font("宋体", Font.PLAIN, 12));
+		orgLabel.setBounds(805,321, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+		add(orgLabel);
+		
+		JLabel phoneLabel=new JLabel("手机号");
+		phoneLabel.setFont(new Font("宋体", Font.PLAIN, 12));
+		phoneLabel.setBounds(805,371, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+		add(phoneLabel);
+		
+		JTextField idField=new JTextField("账号");
+		idField.setFont(new Font("宋体", Font.PLAIN, 12));
+		idField.setBounds(855,121,200,24);
+		add(idField);
+		
+		JTextField passwordField=new JTextField("密码");
+		passwordField.setFont(new Font("宋体", Font.PLAIN, 12));
+		passwordField.setBounds(855,171,200,24);
+		add(passwordField);
+		
+//		JTextField juriField=new JTextField("权限");
+//		juriField.setFont(new Font("宋体", Font.PLAIN, 12));
+//		juriField.setBounds(855,221,200,24);
+//		add(juriField);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"总经理", "高级财务人员", "普通财务人员","快递员","营业厅业务员","中转中心业务员","中转中心仓库管理人员"}));
+		comboBox_1.setFont(new Font("宋体", Font.PLAIN, 12));
+		comboBox_1.setBounds(855, 221, 200, 24);
+		add(comboBox_1);
+		
+		JTextField nameField=new JTextField("姓名");
+		nameField.setFont(new Font("宋体", Font.PLAIN, 12));
+		nameField.setBounds(855,271,200,24);
+		add(nameField);
+		
+		JTextField orgField=new JTextField("机构");
+		orgField.setFont(new Font("宋体", Font.PLAIN, 12));
+		orgField.setBounds(855,321,200,24);
+		add(orgField);
+		
+		JTextField phoneField=new JTextField("手机号");
+		phoneField.setFont(new Font("宋体", Font.PLAIN, 12));
+		phoneField.setBounds(855,371,200,24);
+		add(phoneField);
 
 	}
 }
