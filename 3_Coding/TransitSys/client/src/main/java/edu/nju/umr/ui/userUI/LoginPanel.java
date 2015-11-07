7a2aedbe974cc4db0ea77e934cf0825dbda0984e
EnterpriseCV <502101600@qq.com>
2015-11-07 16:32:47 +0800

    人机交互更新

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/userUI/LoginPanel.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/userUI/LoginPanel.java
@@ -27,16 +27,16 @@ public class LoginPanel extends JPanel {
 		JLabel lblNewLabel = new JLabel("快递物流系统");
 		lblNewLabel.setFont(new Font("宋体", Font.PLAIN, 30));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(364, 10, 242, 67);
+		lblNewLabel.setBounds(414, 10, 242, 67);
 		add(lblNewLabel);
 		
 		JLabel lblNewLabel_3 = new JLabel("账号");
 		lblNewLabel_3.setFont(new Font("宋体", Font.PLAIN, 20));
-		lblNewLabel_3.setBounds(364, 104, 40, 24);
+		lblNewLabel_3.setBounds(414, 104, 40, 24);
 		add(lblNewLabel_3);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(425, 107, 193, 24);
+		textField_1.setBounds(475, 107, 193, 24);
 		add(textField_1);
 		textField_1.setColumns(10);
 		
@@ -46,27 +46,27 @@ public class LoginPanel extends JPanel {
 			}
 		});
 		button.setFont(new Font("宋体", Font.PLAIN, 20));
-		button.setBounds(364, 295, 93, 23);
+		button.setBounds(414, 295, 93, 23);
 		add(button);
 		
 		JButton button_1 = new JButton("关闭");
 		button_1.setFont(new Font("宋体", Font.PLAIN, 20));
-		button_1.setBounds(525, 295, 93, 23);
+		button_1.setBounds(575, 295, 93, 23);
 		add(button_1);
 		
 		JLabel label = new JLabel("密码");
 		label.setFont(new Font("宋体", Font.PLAIN, 20));
-		label.setBounds(364, 162, 40, 24);
+		label.setBounds(414, 162, 40, 24);
 		add(label);
 		
 		textField = new JTextField();
 		textField.setColumns(10);
-		textField.setBounds(425, 162, 193, 24);
+		textField.setBounds(475, 162, 193, 24);
 		add(textField);
 		
 		JButton button_2 = new JButton("物流信息查询");
 		button_2.setFont(new Font("宋体", Font.PLAIN, 20));
-		button_2.setBounds(393, 233, 199, 24);
+		button_2.setBounds(443, 233, 199, 24);
 		add(button_2);
 
 	}
