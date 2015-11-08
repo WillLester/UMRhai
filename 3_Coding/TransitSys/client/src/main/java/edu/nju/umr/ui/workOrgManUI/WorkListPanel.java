41eea53aae26ebcd69c662a6f0a73ecf467f97fa
EnterpriseCV <502101600@qq.com>
2015-11-08 20:21:26 +0800

    界面更新

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/workOrgManUI/WorkListPanel.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/workOrgManUI/WorkListPanel.java
@@ -45,20 +45,28 @@ public class WorkListPanel extends JPanel {
 		add(listTable);
 		
 		JLabel workName = new JLabel("姓名");
-		workName.setBounds(Constants.TABLE_X, listTable.getY()+listTable.getHeight()+20, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT_S);
+		workName.setBounds(Constants.TABLE_X, listTable.getY()+listTable.getHeight()+20,50,24);
 		add(workName);
 		
 		textFieldName = new JTextField();
-		textFieldName.setBounds(workName.getX()+workName.getWidth(), workName.getY()+3,Constants.TEXTFIELD_WIDTH_S, Constants.TEXTFIELD_HEIGHT);
+		textFieldName.setBounds(workName.getX()+workName.getWidth(), workName.getY(),150, 24);
 		add(textFieldName);
-		textFieldName.setColumns(10);
+		
+		JLabel wage = new JLabel("薪水策略");
+		wage.setBounds(700, listTable.getY()+listTable.getHeight()+20,60,24);
+		wage.setEnabled(false);
+		add(wage);
+		
+		JTextField textFieldWage = new JTextField();
+		textFieldWage.setBounds(760, workName.getY(),150, 24);
+		add(textFieldWage);
 		
 		JLabel type = new JLabel("所属机构");
-		type.setBounds(this.getWidth()/2+100, workName.getY(), Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT_S);
+		type.setBounds(380, workName.getY(),60,24);
 		add(type);
 		
 		JComboBox orgType = new JComboBox();
-		orgType.setBounds(type.getWidth()+type.getX(), type.getY()+5, 150, 21);
+		orgType.setBounds(type.getWidth()+type.getX(), type.getY(), 150, 21);
 		add(orgType);
 		
 		JLabel mobile = new JLabel("手机");
@@ -66,12 +74,29 @@ public class WorkListPanel extends JPanel {
 		add(mobile);
 		
 		textFieldMobile = new JTextField();
-		textFieldMobile.setBounds(textFieldName.getX(), mobile.getY()+3, 250, 23);
+		textFieldMobile.setBounds(textFieldName.getX(), mobile.getY(), 150, 24);
 		add(textFieldMobile);
-		textFieldMobile.setColumns(10);
+		
+		JLabel wageNum1 = new JLabel("薪水数额");
+		wageNum1.setBounds(700,mobile.getY(),60,24);
+		wageNum1.setEnabled(false);
+		add(wageNum1);
+		
+		JTextField textFieldWageNum1 = new JTextField();
+		textFieldWageNum1.setBounds(760,mobile.getY(),80, 24);
+		add(textFieldWageNum1);
+		
+		JLabel wageNum2 = new JLabel("份额");
+		wageNum2.setBounds(840,mobile.getY(),60,24);
+		wageNum2.setEnabled(false);
+		add(wageNum2);
+		
+		JTextField textFieldWageNum2 = new JTextField();
+		textFieldWageNum2.setBounds(870,mobile.getY(),40, 24);
+		add(textFieldWageNum2);
 		
 		JLabel position = new JLabel("职位");
-		position.setBounds(type.getX(), type.getY()+type.getHeight()+20, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT_S);
+		position.setBounds(type.getX(), type.getY()+type.getHeight()+20,60,24);
 		add(position);
 		
 		JComboBox comboBox = new JComboBox();
@@ -79,7 +104,7 @@ public class WorkListPanel extends JPanel {
 		add(comboBox);
 		
 		JButton add = new JButton("新增");
-		add.setBounds(listTable.getX()+100, textFieldMobile.getY()+textFieldMobile.getHeight()+30, 93, 23);
+		add.setBounds(listTable.getX(), textFieldMobile.getY()+textFieldMobile.getHeight()+30, 93, 23);
 		add(add);
 		
 		JButton delete = new JButton("删除");
@@ -98,8 +123,12 @@ public class WorkListPanel extends JPanel {
 //		forDetail.setBounds(confirmMod.getX()+confirmMod.getWidth()+50, add.getY(), 93, 23);
 //		add(forDetail);
 		
+		JButton wageMan = new JButton("薪水制定");
+		wageMan.setBounds(confirmMod.getX()+confirmMod.getWidth()+50, add.getY(), 93, 23);
+		add(wageMan);
+		
 		JButton out = new JButton("退出");
-		out.setBounds(confirmMod.getX()+confirmMod.getWidth()+50, add.getY(), 93, 23);
+		out.setBounds(confirmMod.getX()+confirmMod.getWidth()+50+confirmMod.getWidth()+50, add.getY(), 93, 23);
 		add(out);
 		
 		
