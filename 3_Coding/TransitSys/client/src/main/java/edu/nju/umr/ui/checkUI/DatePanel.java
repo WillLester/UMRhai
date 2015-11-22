f9b8da065feea1aa9f3d784fc8fcdd4cdcf1ee0d
EnterpriseCV <502101600@qq.com>
2015-11-22 19:59:35 +0800

    删掉了日期选择面板的测试代码

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/checkUI/DatePanel.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/checkUI/DatePanel.java
@@ -104,13 +104,4 @@ public class DatePanel extends JPanel {
 		}
 		comboBox_2.setModel(new DefaultComboBoxModel(dates[date]));
 	}
-	public static void main(String[] args){
-		JFrame frame=new JFrame();
-		frame.setContentPane(new DatePanel());
-		frame.setSize(500,500);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-
 }
