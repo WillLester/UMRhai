fe3b711d6c763a8a6e5297f362700633e0e2ad6e
yyy2015 <2456700983@qq.com>
2015-11-26 20:15:08 +0800

    单据审批（部分），入款单完善

--- 3_Coding/TransitSys/common/src/main/java/edu/nju/umr/dataService/orderNewDSer/StockInOrderDSer.java
+++ 3_Coding/TransitSys/common/src/main/java/edu/nju/umr/dataService/orderNewDSer/StockInOrderDSer.java
@@ -2,11 +2,14 @@ package edu.nju.umr.dataService.orderNewDSer;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
+import edu.nju.umr.po.ShelfPO;
 import edu.nju.umr.po.enums.Result;
 import edu.nju.umr.po.order.StockInPO;
 
 public interface StockInOrderDSer extends Remote{
 	public Result create(StockInPO order) throws RemoteException;
+	public ArrayList<ShelfPO> getShelves(String orgId) throws RemoteException;
 //	public ArrayList<String> getCities() throws RemoteException;
 }
