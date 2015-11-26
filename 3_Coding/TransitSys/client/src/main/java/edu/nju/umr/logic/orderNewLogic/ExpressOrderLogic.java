ef4fa0a45517dcc78f3544e0205d7695cb5833fd
yyy2015 <2456700983@qq.com>
2015-11-26 21:12:10 +0800

    快递单完善

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logic/orderNewLogic/ExpressOrderLogic.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logic/orderNewLogic/ExpressOrderLogic.java
@@ -4,12 +4,14 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Calendar;
 
 import edu.nju.umr.constants.Url;
 import edu.nju.umr.dataService.dataFactory.ExpressOrderDFacSer;
 import edu.nju.umr.dataService.orderNewDSer.ExpressOrderDSer;
 import edu.nju.umr.logicService.orderNewLogic.ExpressOrderLSer;
 import edu.nju.umr.po.enums.Result;
+import edu.nju.umr.po.order.ExpressPO;
 import edu.nju.umr.vo.order.ExpressVO;
 
 public class ExpressOrderLogic implements ExpressOrderLSer{
@@ -30,7 +32,15 @@ public class ExpressOrderLogic implements ExpressOrderLSer{
 	}
 	public Result create(ExpressVO order) {
 		// TODO 自动生成的方法存根
-		boolean isSuc = false;
+		Result isSuc = Result.SUCCESS;
+		try {
+			isSuc=expressData.create(new ExpressPO(order.getSender(), order.getSendLoc(), order.getReceiver(), order.getReceiveLoc(), order.getSendMobile(), order.getReceiveMobile(),
+					order.getSendPhone(), order.getReceivePhone(), order.getSendUnit(), order.getReceiveUnit(), order.getNum(), order.getName(), order.getLength(), order.getWidth(), order.getHeight(), order.getWeight(), order.getVolumn(), 
+					order.getId(), order.getArrive(), order.getKind(), order.getCost(), Calendar.getInstance(), order.getOpName()));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return Result.NET_INTERRUPT;
+		}
 //		try {
 //			isSuc = expressData.create(new ExpressPO(order.getSender(), order.getSendLoc(), order.getReceiver(), order.getReceiveLoc(), order.getSendMobile(), order.getReceiveMobile(), order.getNum(), order.getName(), order.getLength(), order.getWidth(), order.getHeight(), order.getWeight(), order.getVolumn(), order.getId(), order.getArrive(), order.getKind(), order.getCost()));
 //		} catch (RemoteException e) {
