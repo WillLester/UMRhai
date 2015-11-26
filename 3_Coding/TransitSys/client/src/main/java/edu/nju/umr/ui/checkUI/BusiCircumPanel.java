5af78334e0c4bfdec84a77795d88166f2db3b3b3
EnterpriseCV <502101600@qq.com>
2015-11-26 18:56:33 +0800

    添加导出，完成经营情况表

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/checkUI/BusiCircumPanel.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/checkUI/BusiCircumPanel.java
@@ -1,26 +1,33 @@
 package edu.nju.umr.ui.checkUI;
 
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JFrame;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 
+import edu.nju.umr.logicService.checkLogicSer.BusiCircumLSer;
+import edu.nju.umr.po.enums.Result;
 import edu.nju.umr.ui.Constants;
 import edu.nju.umr.ui.DatePanel;
+import edu.nju.umr.ui.HintFrame;
 import edu.nju.umr.ui.Table;
+import edu.nju.umr.vo.ResultMessage;
+import edu.nju.umr.vo.order.BusiCircumVO;
 /*
  * yyy
  * 20151029
@@ -28,9 +35,15 @@ import edu.nju.umr.ui.Table;
  */
 public class BusiCircumPanel extends JPanel {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JFrame frame;
 	private Table statementTable;
 	private DefaultTableModel model;
+	private ArrayList<BusiCircumVO> orderList;
+	private BusiCircumLSer serv;
 	/**
 	 * Create the panel.
 	 */
@@ -56,12 +69,23 @@ public class BusiCircumPanel extends JPanel {
 		endDate.setBounds(Constants.TABLE_X+Constants.LABEL_WIDTH, Constants.TABLE_Y+Constants.LABEL_HEIGHT_S+4, 267, 21);
 		add(endDate);
 		
-		JButton cancel = new JButton("取消");
-		cancel.setBounds(Constants.PANEL_WIDTH/10*8, Constants.TABLE_Y+Constants.LABEL_HEIGHT_S+4, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
-		add(cancel);
+		JButton exportButton = new JButton("导出");
+		exportButton.setBounds(Constants.PANEL_WIDTH/10*8, Constants.TABLE_Y+Constants.LABEL_HEIGHT_S+4, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+		exportButton.addActionListener(new ActionListener(){@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			export();
+		}});
+		add(exportButton);
 		
 		JButton confirm = new JButton("确认");
 		confirm.setBounds(Constants.PANEL_WIDTH/10*7, Constants.TABLE_Y+Constants.LABEL_HEIGHT_S+4, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
+		confirm.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				getOrders(startDate.getCalendar(),endDate.getCalendar());
+			}
+		});
 		add(confirm);
 		
 //	    statementTable = new JTable();
@@ -91,7 +115,7 @@ public class BusiCircumPanel extends JPanel {
 		tableInit();
 
 	}
-	void tableInit(){
+	private void tableInit(){
 		statementTable = new Table(new DefaultTableModel());
 		model=(DefaultTableModel)statementTable.getModel();
 		statementTable.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
@@ -109,8 +133,77 @@ public class BusiCircumPanel extends JPanel {
 		model.setColumnIdentifiers(columnNames);
 		add(scroll);
 	}
-	void getOrders(){
-		
+	@SuppressWarnings("unchecked")
+	private void getOrders(Calendar start,Calendar end){
+		if(start.after(end))
+		{
+			new HintFrame("时间有误！",frame.getX(),frame.getY(),frame.getWidth(),frame.getHeight());
+			return;
+		}
+		ResultMessage message=serv.getBusiCircum(start, end);
+		Result result=message.getReInfo();
+		if(!result.equals(Result.SUCCESS))
+		{
+			new HintFrame(result,frame.getX(),frame.getY(),frame.getWidth(),frame.getHeight());
+			return;
+		}
+		orderList=(ArrayList<BusiCircumVO>)message.getMessage();
+		displayOrders();
 	}
+	private void displayOrders(){
+		model.setRowCount(0);
+		for(int i=0;i<orderList.size();i++)
+		{
+			BusiCircumVO temp=orderList.get(i);
+			String kind="收款单";
+			if(temp.getKind()==1)kind="入款单";
+			Calendar time=temp.getDate();
+			String timeString=time.get(Calendar.YEAR)+"年"+(time.get(Calendar.MONTH)+1)+"月"+time.get(Calendar.DATE)+"日";
+			String reason=null;
+			if(temp.getRemark().equals(null))
+			{
+				reason="运费收入";
+			}
+			else 
+				switch(temp.getRemark()){
+				case RENT:reason="租金";break;
+				case FREIGHT:reason="运费";break;
+				case WAGE:reason="工资";break;
+				case BONUS:reason="奖励";break;
+				}
+			String[] data=new String[]{kind,timeString,Double.toString(temp.getAmount()),reason};
+			model.addColumn(data);
+		}
+	}
+	private void export(){
+		JFileChooser chooser = new javax.swing.JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("表格","xls","xlsx");
+		chooser.setFileFilter(filter);
+		chooser.setFileSelectionMode(javax.swing.JFileChooser.FILES_ONLY); 
+		int returnVal = chooser.showSaveDialog(null);
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			 File file = chooser.getSelectedFile();
+			 if (file.exists()) {
+			 int copy = JOptionPane.showConfirmDialog(null,"是否要覆盖当前文件？", "保存", JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+			     if (copy == JOptionPane.YES_OPTION){
+			     chooser.approveSelection();
+			     }
+			 }
+			 else
+			 chooser.approveSelection();
+			 Result result= serv.outputExcel(chooser.getSelectedFile().getName(), chooser.getSelectedFile().getParent());
+			 if(!result.equals(Result.SUCCESS))
+			 {
+				 new HintFrame(result,frame.getX(),frame.getY(),frame.getWidth(),frame.getHeight());
+			 }
+		}
+	}
+//	public static void main(String[] args)
+//	{
+//		JFrame frame=new JFrame();
+//		frame.setContentPane(new BusiCircumPanel(frame));
+//		frame.setSize(1200,800);
+//		frame.setVisible(true);
+//	}
 
 }
