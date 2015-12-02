e2eed2fd0e4fd42ba17be1fbd5c8c64635c1c13e
Will Lester <839754614@qq.com>
2015-12-02 22:53:05 +0800

    单据重提交逻辑完毕

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logic/orderApproveLogic/OrderResubmitLogic.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logic/orderApproveLogic/OrderResubmitLogic.java
@@ -5,23 +5,34 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.List;
 
 import edu.nju.umr.constants.Url;
 import edu.nju.umr.dataService.dataFactory.OrderResubmitDFacSer;
 import edu.nju.umr.dataService.orderApproveDSer.OrderResubmitDSer;
 import edu.nju.umr.logic.utilityLogic.VPFactory;
 import edu.nju.umr.logicService.orderApproveLogicSer.OrderResubmitLSer;
+import edu.nju.umr.po.enums.Order;
 import edu.nju.umr.po.enums.Result;
+import edu.nju.umr.po.order.ArrivePO;
+import edu.nju.umr.po.order.CenterLoadingPO;
+import edu.nju.umr.po.order.ExpressPO;
+import edu.nju.umr.po.order.HallLoadingPO;
+import edu.nju.umr.po.order.IncomePO;
 import edu.nju.umr.po.order.KindGetter;
-import edu.nju.umr.po.order.OrderPO;
 import edu.nju.umr.po.order.PaymentPO;
+import edu.nju.umr.po.order.RecipientPO;
+import edu.nju.umr.po.order.SendPO;
+import edu.nju.umr.po.order.StockInPO;
+import edu.nju.umr.po.order.StockOutPO;
+import edu.nju.umr.po.order.TransitPO;
 import edu.nju.umr.vo.ResultMessage;
 import edu.nju.umr.vo.order.OrderVO;
-import edu.nju.umr.vo.order.PaymentVO;
 
 public class OrderResubmitLogic implements OrderResubmitLSer{
 	private OrderResubmitDFacSer dataFac;
 	private OrderResubmitDSer resubmitData;
+	private List<KindGetter> orderList;
 	public OrderResubmitLogic() {
 		try{
 			dataFac = (OrderResubmitDFacSer)Naming.lookup(Url.URL);
@@ -34,43 +45,122 @@ public class OrderResubmitLogic implements OrderResubmitLSer{
             e.printStackTrace();   
         } 
 	}
-	
-	public ResultMessage toRevise(){
-		ArrayList<OrderPO> orderpo=new ArrayList<OrderPO>();
-		ArrayList<OrderVO> ordervo=new ArrayList<OrderVO>();
-		try {
-			orderpo=resubmitData.getRevise();
-			for(OrderPO po:orderpo){
-				OrderVO vo=VPFactory.toOrderVO(po);
-				ordervo.add(vo);
-			}
-			if(orderpo.size()==0){
-				return new ResultMessage(Result.DATA_NOT_FOUND,null);
-			}else
-				return new ResultMessage(Result.SUCCESS,ordervo);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-			return new ResultMessage(Result.NET_INTERRUPT,null);
-		}
-	}
-	
-	public ResultMessage getOrders(String id) {
-		// TODO 自动生成的方法存根
-		ArrayList<KindGetter> orders = null;
-		ArrayList<KindGetter> newOrders = new ArrayList<KindGetter>();
-		try {
-			orders = resubmitData.getOrders(id);
 
-			for(Object order:orders){
-				PaymentPO po = (PaymentPO)order;
-				PaymentVO vo = new PaymentVO( po.getDate(), po.getPayer(), po.getAccount(), po.getKind(), po.getAmount(), po.getRemarks(),po.getOpName());
-				newOrders.add(vo);
-			}
+	@Override
+	public ResultMessage getUnpassed(String id) {
+		// TODO 自动生成的方法存根
+		try {
+			orderList = resubmitData.getOrders(id);
 		} catch (RemoteException e) {
 			// TODO 自动生成的 catch 块
-			e.printStackTrace();
+			return new ResultMessage(Result.NET_INTERRUPT, 0);
 		}
-		return new ResultMessage(Result.SUCCESS, newOrders);
+		return new ResultMessage(Result.SUCCESS, orderList.size());
+	}
+
+	@Override
+	public ResultMessage getOrdersDisplay() {
+		// TODO 自动生成的方法存根
+		ArrayList<OrderVO> orderDisplay = new ArrayList<OrderVO>();
+		for(KindGetter order:orderList){
+			orderDisplay.add(getInfo(order));
+		}
+		return new ResultMessage(Result.SUCCESS, orderDisplay);
+	}
+
+	@Override
+	public ResultMessage getOrder(int index) {
+		// TODO 自动生成的方法存根
+		KindGetter order = orderList.get(index);
+		Order kind = order.getOrderKind();
+		switch(kind){
+		case ARRIVE:
+			ArrivePO poA = (ArrivePO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toArriveVO(poA));
+		case CENTERLOADING:
+			CenterLoadingPO poC = (CenterLoadingPO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toCenterLoadVO(poC));
+		case EXPRESS:
+			ExpressPO poE = (ExpressPO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toExpressVO(poE));
+		case HALLLOADING:
+			HallLoadingPO poH = (HallLoadingPO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toHallLoadingVO(poH));
+		case INCOME:
+			IncomePO poI = (IncomePO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toIncomeVO(poI));
+		case PAYMENT:
+			PaymentPO poP = (PaymentPO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toPaymentVO(poP));
+		case RECIPIENT:
+			RecipientPO poR = (RecipientPO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toRecipientVO(poR));
+		case SEND:
+			SendPO poS = (SendPO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toSendVO(poS));
+		case STOCKIN:
+			StockInPO poSI = (StockInPO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toStockInVO(poSI));
+		case STOCKOUT:
+			StockOutPO poSO = (StockOutPO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toStockOutVO(poSO));
+		case TRANSIT:
+			TransitPO poT = (TransitPO) order;
+			return new ResultMessage(Result.SUCCESS, VPFactory.toTransitVO(poT));
+		}
+		return null;
+	}
+	
+	private OrderVO getInfo(KindGetter order){
+		Order kind = order.getOrderKind();
+		OrderVO result = null;
+		switch(kind){
+		case ARRIVE:
+			ArrivePO poA = (ArrivePO) order;
+			result = new OrderVO(poA.getId(), kind, poA.getOpName(), poA.getOpTime());
+			break;
+		case CENTERLOADING:
+			CenterLoadingPO poC = (CenterLoadingPO) order;
+			result = new OrderVO(poC.getId(), kind, poC.getOpName(), poC.getOpTime());
+			break;
+		case EXPRESS:
+			ExpressPO poE = (ExpressPO) order;
+			result = new OrderVO(poE.getId(), kind, poE.getOpName(), poE.getOpTime());
+			break;
+		case HALLLOADING:
+			HallLoadingPO poH = (HallLoadingPO) order;
+			result = new OrderVO(poH.getId(), kind, poH.getOpName(), poH.getOpTime());
+			break;
+		case INCOME:
+			IncomePO poI = (IncomePO) order;
+			result = new OrderVO(poI.getId()+"", kind, poI.getOpName(), poI.getOpTime());
+			break;
+		case PAYMENT:
+			PaymentPO poP = (PaymentPO) order;
+			result = new OrderVO(poP.getId()+"", kind, poP.getOpName(), poP.getOpTime());
+			break;
+		case RECIPIENT:
+			RecipientPO poR = (RecipientPO) order;
+			result = new OrderVO(poR.getId(), kind, poR.getOpName(), poR.getOpTime());
+			break;
+		case SEND:
+			SendPO poS = (SendPO) order;
+			result = new OrderVO(poS.getId(), kind, poS.getOpName(), poS.getOpTime());
+			break;
+		case STOCKIN:
+			StockInPO poSI = (StockInPO) order;
+			result = new OrderVO(poSI.getId()+"", kind, poSI.getOpName(), poSI.getOpTime());
+			break;
+		case STOCKOUT:
+			StockOutPO poSO = (StockOutPO) order;
+			result = new OrderVO(poSO.getId()+"", kind, poSO.getOpName(), poSO.getOpTime());
+			break;
+		case TRANSIT:
+			TransitPO poT = (TransitPO) order;
+			result = new OrderVO(poT.getId(), kind, poT.getOpName(), poT.getOpTime());
+			break;
+		}
+		return result;
 	}
 
 }
--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logicService/orderApproveLogicSer/OrderResubmitLSer.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logicService/orderApproveLogicSer/OrderResubmitLSer.java
@@ -3,6 +3,25 @@ package edu.nju.umr.logicService.orderApproveLogicSer;
 import edu.nju.umr.vo.ResultMessage;
 
 public interface OrderResubmitLSer {
-	public ResultMessage toRevise();
-	public ResultMessage getOrders(String id);
+	/**
+	 * getUnpassed 获得未通过单据的数量
+	 * <p>  获得未通过单据的数量
+	 * @param id 需要用户名
+	 * @return 返回数量
+	 */
+	public ResultMessage getUnpassed(String id);
+	/**
+	 * getOrdersDisplay 获得未通过单据列表
+	 * <p> 获得未通过单据的列表，形式是OrderVO，参考单据审批的形式
+	 * @param 没有，直接从逻辑层获得
+	 * @return 返回OrderVO的列表
+	 */
+	public ResultMessage getOrdersDisplay();
+	/**
+	 * getOrder 获得某一个未通过的单据
+	 * <p> 获得某一个未通过的单据，形式是某种单据的VO
+	 * @param int index，需要知道在列表中的位置
+	 * @return 返回相应的VO供显示
+	 */
+	public ResultMessage getOrder(int index);
 }
