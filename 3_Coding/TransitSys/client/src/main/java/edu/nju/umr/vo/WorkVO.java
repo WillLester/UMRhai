f5e570e538baa0a0f59596acce319ef72c450698
Will Lester <839754614@qq.com>
2015-12-02 19:59:05 +0800

    人员列表界面完毕

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/vo/WorkVO.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/vo/WorkVO.java
@@ -6,16 +6,14 @@ public class WorkVO {
 	private String name;
 	private String mobile;
 	private String org;
-	private String orgId;
 	private Jurisdiction juri;
 
-	public WorkVO(String name, String mobile, String org, String orgId,
+	public WorkVO(String name, String mobile, String org,
 			Jurisdiction juri) {
 		super();
 		this.name = name;
 		this.mobile = mobile;
 		this.org = org;
-		this.orgId = orgId;
 		this.juri = juri;
 	}
 	public String getName() {
@@ -26,9 +24,6 @@ public class WorkVO {
 	}
 	public String getOrg() {
 		return org;
-	}
-	public String getOrgId() {
-		return orgId;
 	}
 	public Jurisdiction getJuri() {
 		return juri;
