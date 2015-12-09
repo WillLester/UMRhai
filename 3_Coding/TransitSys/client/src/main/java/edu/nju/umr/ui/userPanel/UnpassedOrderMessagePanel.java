fc60db206b4d877204283c0c1ff0e2d5c4de7fb8
Will Lester <839754614@qq.com>
2015-12-09 17:43:56 +0800

    消一个警告

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/userPanel/UnpassedOrderMessagePanel.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/userPanel/UnpassedOrderMessagePanel.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import edu.nju.umr.logic.orderApproveLogic.OrderResubmitLogic;
