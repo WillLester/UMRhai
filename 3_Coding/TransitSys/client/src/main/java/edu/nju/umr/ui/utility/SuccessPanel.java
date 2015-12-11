03f4fe724cd27edf231e66e371bc52966fed9bee
Will Lester <839754614@qq.com>
2015-12-11 17:59:52 +0800

    消去感叹号

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/utility/SuccessPanel.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/utility/SuccessPanel.java
@@ -12,7 +12,7 @@ public class SuccessPanel extends JPanel implements Runnable{
 	 */
 	private static final long serialVersionUID = 7114950449550231951L;
 	private int x,y;
-	private Thread thread;
+	private SuccessThread thread;
 	private JFrame frame;
 	private boolean toClose;
 	public SuccessPanel(JFrame frame){
@@ -24,59 +24,56 @@ public class SuccessPanel extends JPanel implements Runnable{
 		y=frame.getHeight()/6;
 		setBounds(x*6,y*5-50,x,y);
 		toClose=close;
-		thread=new Thread(this);
+		thread=new SuccessThread(this);
 		thread.start();
 	}
-	public void paintComponent(Graphics g)
-	{
+	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.blue);
 		g.fillRect(0,0, x, y);
 		g.setColor(Color.white);
 		g.drawString("操作成功!", 0,60);
 	}
-	public void run()
-	{
+	public void run(){
 		int changeX=x/100;
-		for(int i=0;i<=100;i++)
-		{
-			try
-			{
-				thread.sleep(2);
-			}catch(Exception e)
-			{
+		for(int i=0;i<=100;i++){
+			try{
+				SuccessThread.sleep(2);
+			} catch (InterruptedException e){
 				e.printStackTrace();
 			}
 			this.setBounds(this.getX()-changeX,this.getY(), x, y);
-			if(frame.isVisible())
-			{
+			if(frame.isVisible()){
 				frame.setVisible(true);
 			}
 			repaint();
 		}
 		try{
-			thread.sleep(200);
-		}catch(Exception e)
-		{
+			SuccessThread.sleep(200);
+		} catch (InterruptedException e){
 			e.printStackTrace();
 		}
-		for(int i=0;i<=100;i++)
-		{
-			try
-			{
-				thread.sleep(2);
-			}catch(Exception e)
-			{
+		for(int i=0;i<=100;i++){
+			try {
+				SuccessThread.sleep(2);
+			} catch (InterruptedException e) {
+				// TODO 自动生成的 catch 块
 				e.printStackTrace();
 			}
 			this.setBounds(this.getX()+changeX,this.getY(), x, y);
-			if(frame.isVisible())
-			{
+			if(frame.isVisible()){
 				frame.setVisible(true);
 			}
 			repaint();
 		}
 		frame.remove(this);
 		if(toClose)frame.dispose();
+	}
+	
+	private class SuccessThread extends Thread{
+		public SuccessThread(SuccessPanel panel) {
+			// TODO 自动生成的构造函数存根
+			super(panel);
+		}
 	}
 }
