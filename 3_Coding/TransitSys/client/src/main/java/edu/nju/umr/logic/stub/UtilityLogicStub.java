f198772a21e864e22c9579634a1f2df0f2b2b706
EnterpriseCV <502101600@qq.com>
2015-12-20 21:07:33 +0800

    f@ckyou

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logic/stub/UtilityLogicStub.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logic/stub/UtilityLogicStub.java
@@ -1,19 +1,69 @@
 package edu.nju.umr.logic.stub;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Calendar;
 
+import edu.nju.umr.po.AccountPO;
 import edu.nju.umr.po.CityPO;
+import edu.nju.umr.po.OrgPO;
+import edu.nju.umr.po.StockPO;
+import edu.nju.umr.po.VanPO;
+import edu.nju.umr.po.WorkPO;
 import edu.nju.umr.po.enums.Result;
 import edu.nju.umr.vo.CityVO;
 import edu.nju.umr.vo.ResultMessage;
-public class UtilityLogicStub {
-	public ResultMessage getCities(){
-		ArrayList<CityVO> ar=new ArrayList<CityVO>();
-		ar.add(new CityVO("2","3","4"));
-		return new ResultMessage(Result.SUCCESS,ar);
-	}
-	public ArrayList<CityPO> cities(){
+import edu.nju.umr.dataService.utilityDSer.UtilityDSer;
+public class UtilityLogicStub implements UtilityDSer{
+	public ArrayList<CityPO> getCities() throws RemoteException {
+		// TODO Auto-generated method stub
 		ArrayList<CityPO> ar=new ArrayList<CityPO>();
-		ar.add(new CityPO("2","3","4", 1));
+		ar.add(new CityPO(null, null, null, 0));
 		return ar;
+	}
+	@Override
+	public ArrayList<OrgPO> getOrgs() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<WorkPO> getWorkers(String OrgID) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<VanPO> getVans(String OrgID) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<StockPO> getStocks() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<OrgPO> getHall() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<AccountPO> getAccount() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Result setRecord(Calendar time, String operation, String operator)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<OrgPO> getCenters() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ArrayList<WorkPO> getCouriers(String hallId) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
