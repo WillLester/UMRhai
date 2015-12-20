77451abc355ac2003a04e34e058e0112c8d789b6
yyy2015 <2456700983@qq.com>
2015-12-20 23:09:05 +0800

    frame 一场由大小写引发的灾难

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/component/ExcitedFrame.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/component/ExcitedFrame.java
@@ -2,11 +2,13 @@ package edu.nju.umr.ui.component;
 
 import java.awt.Cursor;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionAdapter;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class ExcitedFrame extends JFrame {
@@ -25,90 +27,52 @@ public class ExcitedFrame extends JFrame {
 	
 	public ExcitedFrame(int width,int height){
 		setLayout(null);
-//		setUndecorated(true);
+		setUndecorated(true);
 		setSize(width,height);
-//		close.setNewImages("close", "closeSt", "closeP");
-//		toMin.setNewImages("mini", "miniSt", "miniP");
-//		toMax.setIcon(new ImageIcon("ui/button/maxP.png"));
-//		
-//		getContentPane().add(close);
-//		getContentPane().add(toMin);
-//		getContentPane().add(toMax);
-//		
-//		close.setBounds(width-50, 0, 50, 50);
-//		toMax.setBounds(width-50*2, 0, 50, 50);
-//		toMin.setBounds(width-50*3, 0, 50, 50);
-//		setVisible(true);
-//		setDragable(this);
-//		close.addActionListener(new ActionListener(){
-//			public void actionPerformed(ActionEvent e) {
-//				System.exit(0);	
-//			}
-//			
-//		});
-//		
-//		toMin.addActionListener(new ActionListener(){
-//			public void actionPerformed(ActionEvent e) {
-//				setExtendedState(ICONIFIED);	
-//			}	
-//		});
+		close.setNewImages("close", "closeSt", "closeP");
+		toMin.setNewImages("mini", "miniSt", "miniP");
+		toMax.setIcon(new ImageIcon("ui/button/maxP.png"));
+		
+		getContentPane().add(close);
+		getContentPane().add(toMin);
+		getContentPane().add(toMax);
+		
+		close.setBounds(width-30, 0, 30, 30);
+		toMax.setBounds(width-30*2, 0, 30, 30);
+		toMin.setBounds(width-30*3, 0, 30, 30);
 		setVisible(true);
-		requestFocus();
-		this.addMouseListener(new MouseListener(){
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("AA");
+		setDragable(this);
+		close.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);	
 			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}});
+			
+		});
+		
+		toMin.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				setExtendedState(ICONIFIED);	
+			}	
+		});
+		
 	}
 	
-	protected void setDragable(final ExcitedFrame ef){
-		System.out.println("Setted");
+	protected void setDragable(ExcitedFrame ef){
 		ef.addMouseListener(new MouseAdapter(){
-			@SuppressWarnings("unused")
-			public void MouseReleased(MouseEvent e){
-				System.out.println("Released");
-				isMoved=false;//鼠标释放后不能进行拖拽
-				ef.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-			}
-			@SuppressWarnings("unused")
-			public void MousePressed(MouseEvent e){
+			public void mouseReleased(MouseEvent e) {  
+                isMoved = false;// 鼠标释放了以后，是不能再拖拽的了  
+                ef.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));  
+            }  
+			public void mousePressed(MouseEvent e){
 				isMoved=true;
-				System.out.println("pressed");
 				pre=new Point(e.getX(),e.getY());//得到鼠标按下的位置
 				ef.setCursor(new Cursor(Cursor.MOVE_CURSOR));
 			}
+			
 		});
 		//拖动时坐标减去鼠标按下去的坐标，是界面要移动的向量
 		this.addMouseMotionListener(new MouseMotionAdapter(){
-			@SuppressWarnings("unused")
-			public void MouseDragged(MouseEvent e){
-				System.out.println("Dragged");
+			public void mouseDragged(MouseEvent e){
 				if(isMoved){
 					end=new Point(ef.getLocation().x+e.getX()-pre.x,ef.getLocation().y+e.getY()-pre.y);
 					ef.setLocation(end);
@@ -116,9 +80,7 @@ public class ExcitedFrame extends JFrame {
 			}
 		});
 	}
-	
-	public static void main(String[] args){
-		ExcitedFrame frame=new ExcitedFrame(1000,700);
-
-	}
+//	public static void main(String[] args){
+//		ExcitedFrame ef=new ExcitedFrame(1200,700);
+//	}
 }
