e204c523f45e72897443af89218b95a4c98b5867
Will Lester <839754614@qq.com>
2015-12-23 23:26:19 +0800

    更新

---
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/component/Utils.java
@@ -0,0 +1,7 @@
+package edu.nju.umr.ui.component;
+
+import java.awt.Color;
+
+public class Utils {
+	public static final Color STD_COLOR = new Color(57, 152,214);
+}
---
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/component/comboBox/UMRComboBoxUI.java
@@ -0,0 +1,7 @@
+package edu.nju.umr.ui.component.comboBox;
+
+import javax.swing.plaf.basic.BasicComboBoxUI;
+
+public class UMRComboBoxUI extends BasicComboBoxUI {
+
+}
