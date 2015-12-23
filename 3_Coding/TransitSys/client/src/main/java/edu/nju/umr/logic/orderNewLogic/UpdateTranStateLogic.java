1576011dcf9fbc53ddf51aa26352f41eebb1d63d
EnterpriseCV <502101600@qq.com>
2015-12-23 17:27:21 +0800

    bie

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logic/orderNewLogic/UpdateTranStateLogic.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/logic/orderNewLogic/UpdateTranStateLogic.java
@@ -4,15 +4,15 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.nju.umr.constants.Url;
 import edu.nju.umr.dataService.dataFactory.orderNew.UpdateTranStateDFacSer;
 import edu.nju.umr.dataService.orderNewDSer.UpdateTranStateDSer;
-import edu.nju.umr.po.enums.Express;
-import edu.nju.umr.po.enums.Parse;
 import edu.nju.umr.po.enums.Result;
 import edu.nju.umr.po.order.ExpressPO;
+import edu.nju.umr.vo.ResultMessage;
 public class UpdateTranStateLogic{
 	UpdateTranStateDFacSer dataFac;
 	UpdateTranStateDSer updateData;
@@ -36,5 +36,18 @@ public class UpdateTranStateLogic{
 		}catch(RemoteException e){
 			return Result.NET_INTERRUPT;
 		}
+	}
+	public ResultMessage getExpressHere(String nowOrgId){
+		ExpressPO ex=new ExpressPO(null,null,null,null,null,null,null,null,null,null,0,null,0,
+				0,0,0,0,null,null,null,null,0, null, null, null, null,null,null,null,null,nowOrgId);
+		List<String> list=new ArrayList<String>();
+		try{
+			list=updateData.getOrdersHere(ex);
+		}catch(RemoteException e){
+			return new ResultMessage(Result.NET_INTERRUPT,null);
+		}
+		ArrayList<String> ar =new ArrayList<String>();
+		ar.addAll(list);
+		return new ResultMessage(Result.SUCCESS,ar);
 	}
 }
