3cc60c83b43986658f6439bb59d514c8d37e7a5f
Will Lester <839754614@qq.com>
2015-12-27 21:39:28 +0800

    小小的得罪

---
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/component/table/UMRTableCellRenderer.java
@@ -0,0 +1,17 @@
+package edu.nju.umr.ui.component.table;
+
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class UMRTableCellRenderer implements TableCellRenderer{
+
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value,
+			boolean isSelected, boolean hasFocus, int row, int column) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+}
