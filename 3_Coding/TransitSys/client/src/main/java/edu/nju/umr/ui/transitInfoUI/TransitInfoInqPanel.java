7583922a0e2c2c4f2fd65fa62e4769ba38ba6285
EnterpriseCV <502101600@qq.com>
2015-12-29 20:56:37 +0800

    ha

--- 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/transitInfoUI/TransitInfoInqPanel.java
+++ 3_Coding/TransitSys/client/src/main/java/edu/nju/umr/ui/transitInfoUI/TransitInfoInqPanel.java
@@ -28,6 +28,7 @@ import edu.nju.umr.ui.component.button.ConfirmButton;
 import edu.nju.umr.ui.component.button.ExitButton;
 import edu.nju.umr.ui.userUI.LoginPanel;
 import edu.nju.umr.ui.utility.CheckLegal;
+import edu.nju.umr.ui.utility.DoHint;
 import edu.nju.umr.vo.ResultMessage;
 
 public class TransitInfoInqPanel extends JPanel {
@@ -86,6 +87,9 @@ public class TransitInfoInqPanel extends JPanel {
 						for(String inf:info){
 							textArea.append(inf);
 							textArea.append("\n");
+						}
+						if(info.size()==0){
+							DoHint.hint(Result.EXPRESS_NOT_FOUND, frame);
 						}
 					} else {
 						@SuppressWarnings("unused")
